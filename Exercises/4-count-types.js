2451af1177905373c744842de326d55eded90fab
Dmytro Dvoretskyi <dvoretskyi.dy@gmail.com>
2022-04-15 15:20:45 +0300

    The tests were passed.

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = (arr) => {
+  const res = {};
+  for (const el of arr) {
+    const type = typeof el;
+    const count = res[type] || 0;
+    res[type] = count + 1;
+  }
+  return res;
+};
 
 module.exports = { countTypesInArray };
